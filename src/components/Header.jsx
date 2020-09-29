d648508857d00eb54d36f3d7de336b5f0eae819f
odowilson <58129855+odowilson@users.noreply.github.com>
2020-09-29 16:33:13 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <LibraryBooksIcon /> Note.io
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import DeleteSweepIcon from "@material-ui/icons/DeleteSweep";
+
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>
+        <DeleteSweepIcon />
+      </button>
+    </div>
+  );
+}
+
+export default Note;
